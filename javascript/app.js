443be160044167953b1ff606e193aeff23087f3b
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-09 22:42:26 +0100

    Add nav menu

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+//nav code in plain js
+var nav = document.getElementById('nav');
+var navToggle = document.getElementById('nav-toggle');
+navToggle.addEventListener('click', function() {
+    nav.classList.toggle('open');
+    }
+);
+
+
