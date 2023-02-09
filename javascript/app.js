542db0d0d19fa7f3e93220ac7681287ecc5da90e
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-09 22:55:52 +0100

    Fix js lint erors

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,6 @@
-//nav code in plain js
-const nav = document.querySelector('nav')
-const navToggle = document.querySelector('.menu-bars')
-navToggle.addEventListener('click', function() {
-    nav.classList.toggle('open');
-    }
-);
-
-
+// nav code in plain js
+const nav = document.querySelector('nav');
+const navToggle = document.querySelector('.menu-bars');
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
